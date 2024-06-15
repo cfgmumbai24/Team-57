289719ae9fc2e25b942616a2cafa1ae87647cfe1
Tanay Gada <tanaygada14@gmail.com>
2024-06-16 02:15:14 +0530

    login logout completed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,6 @@ const validator = require('validator')
 
 const Schema = mongoose.Schema
 
-
 const userSchema = new Schema(
   {
     fullName: {
@@ -12,83 +11,99 @@ const userSchema = new Schema(
       required: true,
     },
     role: {
+      type: String,
       enum: ['admin', 'volunteer'],
       required: true,
-
     },
-    villageAssigned :{
+    villageAssigned: {
       type: String,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: 'Invalid email format',
+      },
     },
-    password:{
+    password: {
       type: String,
-      required: true
+      required: true,
+      validate: {
+        validator: (value) =>
+          validator.isStrongPassword(value, { minLength: 8 }),
+        message:
+          'Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character',
+      },
     },
-    
   },
   { timestamps: true }
 )
 
-
-//static signup method
-userSchema.statics.signup = async function (fullName, email, password, role, villageAssigned) {
-  if (!email || !password || !fullName || !role ) {
-    
-    throw Error('All fields must be filled')
+// Static method for user signup
+userSchema.statics.signup = async function (
+  fullName,
+  email,
+  password,
+  role,
+  villageAssigned
+) {
+  if (!email || !password || !fullName || !role) {
+    throw new Error('All fields must be filled')
   }
+
   if (!validator.isEmail(email)) {
-    throw Error('Email not valid')
+    throw new Error('Invalid email format')
   }
 
   if (!validator.isStrongPassword(password)) {
-    throw Error('Password not strong enough')
+    throw new Error('Password not strong enough')
   }
 
   const emailExists = await this.findOne({ email })
 
   if (emailExists) {
-    throw Error('Email already in use')
+    throw new Error('Email already in use')
   }
 
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
-  const villageAssign = villageAssigned || "Admin"
+  const villageAssign = villageAssigned || 'Admin'
 
-  const user = await this.create({ fullName, email, role, villageAssign, password: hash })
+  const user = await this.create({
+    fullName,
+    email,
+    role,
+    villageAssigned: villageAssign,
+    password: hash,
+  })
 
   return user
 }
 
-
-
-//static login method
-userSchema.statics.login = async function(email,password){
-
-  if (!email || !password ) {
-    throw Error('All fields must be filled')
+// Static method for user login
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw new Error('All fields must be filled')
   }
 
-  const user = await this.findOne({email})
+  const user = await this.findOne({ email })
 
-  if(!user){
-    throw Error(`User Doesn't Exists`)
+  if (!user) {
+    throw new Error(`User with email ${email} does not exist`)
   }
 
   const match = await bcrypt.compare(password, user.password)
 
-  if(!match){
-    throw Error(`Incorrect Password`)
+  if (!match) {
+    throw new Error('Incorrect Password')
   }
 
   return user
 }
 
-
-//Model
-const User = mongoose.model('user', userSchema)
+// Model
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,29 +1,59 @@
 import { useState } from "react"
 import { useLogin } from "../../hooks/useLogin"
+import "../Login/Login.css"
+import { Link } from 'react-router-dom'
 
-const Login = ()=>{
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
-    const {login, error, isLoading} = useLogin()
+const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { login, error, isLoading } = useLogin()
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
-
-        await login(email,password)
-        console.log(error)
-    }
-
-    return(
-        <form onSubmit={handleSubmit} className="signup">
-            <h3>Login</h3>
-            <label >Email</label>
-            <input type="email" onChange={(e)=> setEmail(e.target.value)}/>
-            <label>Password</label>
-            <input type="password" onChange={(e)=> setPassword(e.target.value)}/>
-            <button disabled={isLoading}>Login</button>
-        {error && <p>{error}</p>}
-        </form>
-    )
+    await login(email, password)
+    console.log(error)
+  }
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>Login</div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+        <div className='input'>
+          <i className='fa-solid fa-envelope' />
+          <input
+            type='email'
+            className='user_input'
+            placeholder='Email id'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='input'>
+          <i className='fa-solid fa-lock' />
+          <input
+            type='password'
+            className='user_input'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+      {error && <div className='error-message'>{error}</div>}
+      <div className='forget-pswd'>
+        Forget Password? <span>Click Here</span>
+      </div>
+      <div className='submit' onClick={handleSubmit}>
+        Submit
+      </div>
+      <div className='switch-container'>
+        Don't have an account? <Link to='/signup'>Signup</Link>
+      </div>
+    </div>
+  )
 }
+
 export default Login
